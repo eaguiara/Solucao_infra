fdccb683f4d4adea66368bb4db35a4565314c390
Eduarda Aguiar <73914795+eaguiara@users.noreply.github.com>
2021-03-08 12:24:20 -0300

    Add files via upload

---
+++ Solução_infra.ps1
@@ -0,0 +1,39 @@
+﻿#Recebe os dois arquivos contento a hash que seram comparados
+$var = (Get-FileHash -Algorithm MD5 '').hash
+$var2 = (Get-FileHash -Algorithm MD5 '').hash
+
+if($var -ne($var2)){
+#Mensagem que aparecerá na tela se os arquivos forem diferentes e depois enviará o email
+echo 'São diferentes'
+
+#E-mail do rementente
+$EmailFrom = “”
+
+#E-mail do destinatário
+$EmailTo = “”
+
+#Assunto
+$Subject = “”
+
+#Corpo do e-mail que está pedindo a comparação de dois arquivos
+$Body = “'Compare-Object $var $var2'”
+
+#Endereço do e-mail
+$SMTPServer = “smtp.live.com” #substitua esse endereço pelo endereço do servido SMTP que for utilizar
+
+#Portal do servidor SMTP
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+
+$SMTPClient.EnableSsl = $true
+
+#Recebe um objeto contendo e-mail do remetente e a senha do email
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(“#email”, “#senha”);
+
+#Realizando o envio da mensagem
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+
+
+}else{
+#Mensagem que aparecerá na tela se os arquivos forem iguais
+echo 'são iguais'
+}
---
+++ criar arquivos.ps1
@@ -0,0 +1,3 @@
+﻿Get-FileHash -Algorithm SHA256 'C:\Users\eange\OneDrive\Documentos\hash\teste_hash\TESTE.txt' |out-file -encoding UTF8 -Width 200 -File C:\Users\eange\OneDrive\Documentos\hash\temp_hash\RESUL10.txt
+Get-Content C:\Users\eange\OneDrive\Documentos\hash\temp_hash\RESUL10.txt | Select -Index 3 > C:\Users\eange\OneDrive\Documentos\hash\temp_hash\RESUL20.txt
+Get-Content C:\Users\eange\OneDrive\Documentos\hash\temp_hash\RESUL20.txt | out-file -encoding UTF8 C:\Users\eange\OneDrive\Documentos\hash\original1.txt
